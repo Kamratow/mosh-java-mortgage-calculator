58cde23cfda94b5a13a69b5a30007ee2f8c4c48a
Kamil Ratowski <kamilratowski90@gmail.com>
2024-07-23 22:54:12 +0200

    Created initial implementation for mortgage calculator

--- src/com/helloworld/Main.java
+++ src/com/helloworld/Main.java
@@ -1,9 +1,29 @@
 package com.helloworld;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        float x = 1;
-        x /= 2;
-        System.out.println(x);
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Principal: ");
+        int principal = scanner.nextInt();
+
+        System.out.print("Annual Interest Rate: ");
+        float annualInterestRate = scanner.nextFloat();
+        float monthlyInterestRate = annualInterestRate/PERCENT/MONTHS_IN_YEAR;
+
+        System.out.print("Period (Years): ");
+        byte years = scanner.nextByte();
+        int numberOfPayments = years * 12;
+
+        String mortgage = NumberFormat.getCurrencyInstance(Locale.US)
+                .format(principal * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numberOfPayments))/(Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1));
+        System.out.println("Mortgage: " + mortgage);
     }
 }
