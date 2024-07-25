c17e92d419a7f54e778b355da6f9f3703e78e744
Kamil Ratowski <kamilratowski90@gmail.com>
2024-07-25 15:10:44 +0200

    Removed FizzBuzz commented code as it won't be used anymore

--- src/com/helloworld/Main.java
+++ src/com/helloworld/Main.java
@@ -15,21 +15,6 @@ public class Main {
         years = (byte)readNumber("Period (Years): ", 1, 30);
 
         System.out.println("Mortgage: " + calculateMortgage(principal, annualInterestRate, years));
-
-//        FizzBuzz exercise solution
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.print("Number: ");
-//        int number = scanner.nextInt();
-//
-//        if (number % 5 == 0 && number % 3 == 0) {
-//            System.out.println("FizzBuzz");
-//        } else if (number % 5 == 0) {
-//            System.out.println("Fizz");
-//        } else if (number % 3 == 0) {
-//            System.out.println("Buzz");
-//        } else {
-//            System.out.println(number);
-//        }
     }
 
     public static double readNumber(String prompt, double min, double max) {
