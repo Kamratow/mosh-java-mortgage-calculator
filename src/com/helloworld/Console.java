2b5dc552f6a6eff70c183c3e57771eab4eea7158
Kamil Ratowski <kamilratowski90@gmail.com>
2024-07-26 16:57:45 +0200

    Overloaded readNumber method and extracted scanner to a field of Console class

--- src/com/helloworld/Console.java
+++ src/com/helloworld/Console.java
@@ -3,8 +3,14 @@ package com.helloworld;
 import java.util.Scanner;
 
 public class Console {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static double readNumber(String prompt) {
+        System.out.print(prompt);
+        return scanner.nextDouble();
+    }
+
     public static double readNumber(String prompt, double min, double max) {
-        Scanner scanner = new Scanner(System.in);
         double value;
 
         while (true) {
